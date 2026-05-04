ccb01ae55b7229e920c5dc0445b1a41b369efe5c
Daniil <crazydevstuck@gmail.com>
2026-05-04 16:18:43 +0300

    fix: update dependencies for vercel deployment

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,24 @@
-import js from "@eslint/js";
-import globals from "globals";
+import globals from 'globals';
 
 export default [
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
-        bootstrap: "readonly",
+        bootstrap: 'readonly',
       },
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     rules: {
-      "no-console": "off",
-      "semi": ["error", "always"],
-      "quotes": ["error", "single"],
-      "indent": ["error", 2],
-      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      'no-console': 'off',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
+      'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+      'no-undef': 'error',
     },
   },
 ];
